a1702d4267214027f59c244fc5ba8b9d52d1f7c3
Andre Minelli <andreminelli@gmail.com>
2017-03-01 17:45:11 -0300

    Add Sample

---
+++ Sample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Take.Blip.FlowBuilder
+{
+    class Sample
+    {
+        var optionsFlow = FlowBuilder.Start()
+                            .WhenReceived(MatchTextRegEx("1|Extrato"), SetHandler<NoteHandler>())
+                            .WhenReceived(MatchTextRegEx("2|Saldo"), SetHandler<AmountHandler>())
+                            .WhenReceived(MatchTextRegEx("3|Voltar"), SetHandler<AmountHandler>());
+        var mainFlow = FlowBuilder.Start()
+                        .WhenReceived(MatchTextRegEx(""), Send(""))
+                        .WhenReceived(MatchTextRegEx(""), Send(""), Start(optionsFlow))
+                        .Otherwise(Send(""));
+    }
+}
